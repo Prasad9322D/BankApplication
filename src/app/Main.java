367a8d4ccac9298c8a4f616ce1a7704d4799e3b8
Prasad <prasaddeshamukh01@gmail.com>
2025-10-25 19:15:11 +0530

    Structure Done

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,18 +1,30 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
         System.out.println("Welcome to Banking Application");
-        System.out.println("""
-                1) open Account
-                2) Deposit
-                3) Withdraw
-                4) Transfer
-                5) Account Statement
-                6) List Account
-                7) Search Accounts by Customer Name
-                8) Exit
-            """);
+        while(running) {
+            System.out.println("""
+                        1) open Account
+                        2) Deposit
+                        3) Withdraw
+                        4) Transfer
+                        5) Account Statement
+                        6) List Account
+                        7) Search Accounts by Customer Name
+                        8) Exit
+                    """);
+            System.out.print("Choose: ");
+            String choice = scanner.nextLine();
+
+            switch (choice){
+                case "0" -> running = false;
+            }
+        }
     }
 }
