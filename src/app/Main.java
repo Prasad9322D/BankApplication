b1074f397ff91722084a8ed99eb9426c7ee7a888
Prasad <prasaddeshamukh01@gmail.com>
2025-10-25 18:56:00 +0530

    Update

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,9 +1,18 @@
 package app;
 
 public class Main {
-    static void main(String[] args) {
+    public static void main(String[] args) {
 
-        System.out.println("Hello World");
-        System.out.println("Hello World");
+        System.out.println("Welcome to Banking Application");
+        System.out.println("""
+                1) open Account
+                2) Deposit
+                3) Withdraw
+                4) Transfer
+                5) Account Statement
+                6) List Account
+                7) Search Accounts by Customer Name
+                8) Exit
+            """);
     }
 }
